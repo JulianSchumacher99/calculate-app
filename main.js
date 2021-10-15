3c0bfbbc1b15685ed95adb1c5433dbadc145fec4
JulianSchumacher99 <julian.schumacher@sterntaler.com>
2021-10-15 11:33:02 +0200

    Added HTML structure and included a img an pagetitle via JavaScript

---
+++ main.js
@@ -0,0 +1,21 @@
+//import { createCalculatePage } from "./lib/calculatePage.js";
+import { createPageTitle } from "./lib/titleComponent.js";
+import { createPageImg } from "./lib/titleComponent.js";
+
+function createApp() {
+  const appElement = document.querySelector("#app");
+
+  const pageTitle = createPageTitle();
+  const pageImg = createPageImg();
+
+  appElement.append(pageImg);
+  appElement.append(pageTitle);
+
+  const enterCalculatePage = createCalculatePage(function (total) {
+    alert(`Dein Ergebnis ist ${total}.`);
+  });
+
+  appElement.append(enterCalculatePage);
+}
+
+createApp();
